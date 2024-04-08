fda3fdafd69fe9af52f64d832992c0effc94954f
효석 이 <lhs19960330@gmail.com>
2024-04-09 07:35:19 +0900

    feat : 미로 플레이 중 벽 충돌

--- Assets/Lee/Maze/MazePlay.cs
+++ Assets/Lee/Maze/MazePlay.cs
@@ -3,8 +3,22 @@ using UnityEngine.EventSystems;
 
 public class MazePlay : MonoBehaviour, IDragHandler
 {
+    public float moveDistance = 1.0f; // 한번에 이동할 거리
+
     public void OnDrag( PointerEventData eventData )
     {
-        transform.position = new Vector3(eventData.pointerCurrentRaycast.worldPosition.x, transform.position.y, eventData.pointerCurrentRaycast.worldPosition.z);
+        Vector3 targetPosition = new Vector3(eventData.pointerCurrentRaycast.worldPosition.x, transform.position.y, eventData.pointerCurrentRaycast.worldPosition.z);
+        Vector3 direction = targetPosition - transform.position;
+        direction.Normalize();
+
+        RaycastHit hit;
+        if ( Physics.Raycast(transform.position, direction, out hit, moveDistance) )
+        {
+            // 벽과 충돌하므로 이동하지 않음
+            return;
+        }
+
+        // 벽과 충돌하지 않으므로 이동
+        transform.position = targetPosition;
     }
 }
