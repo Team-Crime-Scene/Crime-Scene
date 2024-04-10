34c2c456b13ec8f48cb29db3464d8dff26fe1915
효석 이 <lhs19960330@gmail.com>
2024-04-10 12:48:58 +0900

    feat : 답안지쓸때 나가기와 캡쳐안되게하기

--- Assets/Lee/_ScriptsRe/Interact/ComputerUI.cs
+++ Assets/Lee/_ScriptsRe/Interact/ComputerUI.cs
@@ -108,4 +108,8 @@ public class ComputerUI : PopUpUI
     {
         Manager.Game.ChangeIsChatTrue();
     }
+    public void DisableInputField()
+    {
+        Manager.Game.ChangeIsChatFalse();
+    }
 }
