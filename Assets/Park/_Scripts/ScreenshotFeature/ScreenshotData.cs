120cc03721049f8945a43042020dd6bc4a45e0a7
Kashmir <pj93251428@gmail.com>
2024-04-12 09:55:40 +0900

    fix
    :wrench:  Screenshot 객체에서 monobehavior 상속 제거(...)
    
    :wrench:  ScreenshotData 생성시 new 키워드 대신 Extension에서 createInstance 사용하게 :factory:  팩토리 패턴 적용

--- Assets/Park/_Scripts/ScreenshotFeature/ScreenshotData.cs
+++ Assets/Park/_Scripts/ScreenshotFeature/ScreenshotData.cs
@@ -14,5 +14,4 @@ public class ScreenshotData :ScriptableObject
     public string path;
     public bool isBookmarked;
     //public string imageTag;  // Screenshot이 가진 tag로 정답을 판별하고자 할 때 사용 
-    public GameObject prefab; // 화이트 보드에 붙일 Prefab, 쓸수도있고 아닐수도있고...
 }
